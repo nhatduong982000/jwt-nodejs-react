12dd9a1799c289e5c6c3f82b2352351cd63cda32
Nhat Duong <18520650@gm.uit.edu.vn>
2022-10-01 22:28:41 +0700

    mvc

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,15 +1,17 @@
 import  express  from "express";
-
+import homeController from "../controller/homeController";
 const router = express.Router();
 /**
  * 
  * @param {*} app: express app 
  */
+
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res)=> {
-        return res.send("hello world");
-    })
-    return app.use("/", router);
+    router.get("/", homeController.handleHelloWorld)
+    router.get("/about", homeController.handleUserPage)
+    return app.use("/", router); // tức là ứng dụng sẽ bắt đầu với cái dấu "/"
+                                //vd khi /abc thì ứng dụng sẽ chạy là localhost:port/abc/about
 }
 
 export default initWebRoutes;
