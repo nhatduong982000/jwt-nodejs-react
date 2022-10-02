6fc0b0bec706a57c96902674017ab3a00ff91208
Nhat Duong <18520650@gm.uit.edu.vn>
2022-10-02 22:16:11 +0700

    form boostrap 5

--- src/routers/web.js
+++ src/routers/web.js
@@ -9,9 +9,9 @@ const router = express.Router();
 
 const initWebRoutes = (app) => {
     router.get("/", homeController.handleHelloWorld)
-    router.get("/about", homeController.handleUserPage)
+    router.get("/user", homeController.handleUserPage)
     return app.use("/", router); // tức là ứng dụng sẽ bắt đầu với cái dấu "/"
-                                //vd khi /abc thì ứng dụng sẽ chạy là localhost:port/abc/about
+                                //  vd khi /abc thì ứng dụng sẽ chạy là localhost:port/abc/about
 }
 
 export default initWebRoutes;
